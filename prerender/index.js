774b678fee1412cc442cab0fdfba36eb09dcb93e
Varunjaishreeram <varunbharadwaj444@gmail.com>
2025-11-16 00:59:26 +0530

    correct

--- prerender/index.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  window.dispatchEvent(new Event("render-event"));
-});
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import prerender from 'vite-plugin-prerender'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-
-    // Copy prerender script into dist/
     viteStaticCopy({
       targets: [
         {
-          src: 'prerender/index.js',
-          dest: 'prerender'
+          src: 'prerender/index.html',
+          dest: ''
         }
       ]
-    }),
-
-    // Prerender main page
-    prerender({
-      routes: ['/'],
-      renderAfterDocumentEvent: 'render-event'
     })
   ]
 })
